07aff420ac6c94d720f4c2a538da34fee7e85302
Alex Edwards <lensgeek92@gmail.com>
2020-05-21 17:20:25 +0100

    Fixed API conflicts

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -5,12 +5,3 @@ const Layout = require('./src/components/Layout').default
 exports.wrapPageElement = ({element, props}) => {
   return <Layout {...props}>{element}</Layout>
 }
-
-exports.onClientEntry = () => {
-  const vh = window.innerHeight * 0.01
-  document.documentElement.style.setProperty('--vh', `${vh}px`)
-  window.addEventListener('resize', () => {
-    const vh = window.innerHeight * 0.01
-    document.documentElement.style.setProperty('--vh', `${vh}px`)
-  })
-}
